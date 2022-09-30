f714f0d8d85ff0d59f7287b61a7f6ee311634005
LiubomyrPapushak <114106927+LiubomyrPapushak@users.noreply.github.com>
2022-09-30 15:53:00 +0300

    Add files via upload

---
+++ Source.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<locale.h>
+#include <conio.h>
+#include <math.h>
+
+int a, b, rez1, rez2, rez3;
+
+int main()
+{
+	setlocale(LC_CTYPE, "ukr");
+	printf("\n ¬ведiть два цiлих числа: \n");
+	printf("\n \t введiть число a=");
+	scanf("%d", &a);
+	printf("\n \t ¬ведiть вiдмiнне вiд 0 число b=");
+	scanf("%d", &b);
+
+	rez1 = a + b;
+	rez2 = b % a; 
+	rez3 = a * b;
+
+	printf("\n \n ќ–»ћјЌ»… –≈«”Ћ№“ј“:");
+	printf("\n \t сума чисел %d + %d = %d", a, b, rez1);
+	printf("\n \t результат д≥ленн€ %d / %d = %d", b, a, rez2);
+	printf("\n \t добуток чисел %d * %d = %d", a, b, rez3);
+	int getch(); getch();
+
+	return 0;
+}
